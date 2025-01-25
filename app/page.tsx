d74cc37e16cf9f8cb26646a3644741f90985ee69
meytizn <zamanianmehdi23desktop@gmail.com>
2025-01-25 22:31:42 +0330

    projects started with desktop design completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,208 @@
 import Image from 'next/image'
+import { Fragment } from 'react'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <Fragment>
+      <div id="nabvar" className="flex flex-row w-[100%] md:w-[100%] bg-[#121212]  h-[56px] text-white float-right justify-center items-center gap-x-2 ">
+        
+      <div id="row1" className="flex flex-row w-[100px] h-[40px] text-white text-[25px] text-blue-500 ">مووی کو </div>
+      <div id="row2" className="flex flex-row w-[100px] h-[40px] text-white text-[25px] bg-blue-500 ">مووی کو </div>
+      <div id="row3" className="flex flex-row w-[100px] h-[40px] text-white text-[25px] bg-blue-500 ">مووی کو </div>
+      <div id="row4 serach" className="flex flex-row w-[50%] h-[40px] text-white text-[25px] bg-red-500 ">مووی کو </div>
+
+      <div id="row5" className="flex flex-row w-[100px] h-[40px] text-white text-[25px] bg-blue-500 ">مووی کو </div>
+      <div id="row6" className="flex flex-row w-[100px] h-[40px] text-white text-[25px] bg-blue-500 ">مووی کو </div>
+      
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+{/* 
+imageslider section */}
+
+<div id="imageslider" className="w-[100%] h-[136px] md:h-[330px] bg-blue-500">
+</div>
+
+
+{/* 
+end imageslider section */}
+
+
+
+
+
+
+
+
+
+
+<div id="contenter" className=" flex flex-row justify-center w-[100%] h-[136px] md:h-[530px] bg-red-500 gap-x-3 mt-1">
+
+<div id="row1" className="flex flex-row w-[26%] md:h-[530px] text-white text-[25px] bg-green-500 ">مووی کو </div>
+<div id="row1" className="flex flex-row w-[55%] md:h-[530px] text-white text-[25px] bg-pink-500 ">مووی کو </div>
+      
+</div>
+
+
+
+
+
+{/* 
+movie section */}
+
+<div id="movielist master container" className="flex flex-row gap-y-2 flex-wrap justify-between w-[100%]  text-white text-[25px] bg-gray-800 px-[8%] py-2 my-2 "> برترین فیلم ها   </div>
+
+<div id="movielist master" className=" flex flex-row justify-center w-[100%]  bg-yellow-500 gap-x-3 mt-1">
+
+<div id="movielist master container" className="flex flex-row gap-y-2 flex-wrap justify-between w-[90%]  text-white text-[25px] bg-green-500 "> 
+
+<div id="item1" className="flex flex-row w-[220px] md:h-[430px] text-white text-[25px] bg-black "> 1</div>
+<div id="item2" className="flex flex-row w-[220px] md:h-[430px] text-white text-[25px] bg-black "> 1</div>
+<div id="item3" className="flex flex-row w-[220px] md:h-[430px] text-white text-[25px] bg-black "> 1</div>
+<div id="item4" className="flex flex-row w-[220px] md:h-[430px] text-white text-[25px] bg-black "> 1</div>
+<div id="item5" className="flex flex-row w-[220px] md:h-[430px] text-white text-[25px] bg-black "> 1</div>
+<div id="item6" className="flex flex-row w-[220px] md:h-[430px] text-white text-[25px] bg-black "> 1</div>
+
+
+
+
+</div>
+
+
+
+
+</div>
+
+
+
+{/* 
+end movie section */}
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* 
+actors section */}
+
+<div id="actors master container" className="flex flex-row gap-y-2 flex-wrap justify-between w-[100%]  text-white text-[25px] bg-gray-800 px-[8%] py-2 my-2 "> بهترین بازیگران سال   </div>
+
+<div id="actors master" className=" flex flex-row justify-center w-[100%]  bg-yellow-500 gap-x-3 mt-1">
+
+<div id="actors master container" className="flex flex-row gap-y-2 flex-wrap justify-between w-[90%]  text-white text-[25px] bg-green-500 "> 
+
+<div id="item1" className="flex flex-row w-[220px] md:h-[230px] text-white text-[25px] bg-black rounded-[120px]"> 1</div>
+<div id="item2" className="flex flex-row w-[220px] md:h-[230px] text-white text-[25px] bg-black rounded-[120px]"> 1</div>
+<div id="item3" className="flex flex-row w-[220px] md:h-[230px] text-white text-[25px] bg-black rounded-[120px]"> 1</div>
+<div id="item4" className="flex flex-row w-[220px] md:h-[230px] text-white text-[25px] bg-black rounded-[120px]"> 1</div>
+<div id="item5" className="flex flex-row w-[220px] md:h-[230px] text-white text-[25px] bg-black rounded-[120px]"> 1</div>
+<div id="item6" className="flex flex-row w-[220px] md:h-[230px] text-white text-[25px] bg-black rounded-[120px]"> 1</div>
+
+
+
+
+</div>
+
+
+
+
+</div>
+
+
+{/* 
+end actors section */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* 
+feature section */}
+
+<div id="feature master container" className="flex flex-row gap-y-2 flex-wrap justify-between w-[100%]  text-white text-[25px] bg-gray-800 px-[8%] py-2 my-2 "> منتخب های امروز  </div>
+
+<div id="feature master" className=" flex flex-row justify-center w-[100%]  bg-yellow-500 gap-x-3 mt-1">
+
+<div id="feature master container" className="flex flex-row gap-y-2 flex-wrap justify-between w-[90%]  text-white text-[25px] bg-green-500 "> 
+
+<div id="item1" className="flex flex-row w-[31.3%] md:h-[230px] text-white text-[25px] bg-black rounded-[10px]"> 1</div>
+<div id="item2" className="flex flex-row w-[31.3%] md:h-[230px] text-white text-[25px] bg-black rounded-[10px]"> 1</div>
+<div id="item3" className="flex flex-row w-[31.3%] md:h-[230px] text-white text-[25px] bg-black rounded-[10px]"> 1</div>
+
+
+
+
+</div>
+
+
+
+
+</div>
+
+
+{/* 
+end feature section */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* 
+footer section */}
+
+<div id="footer" className="w-[100%] h-[136px] md:h-[130px] bg-blue-500 my-2">
+</div>
+
+
+{/* 
+end footer section */}
+
+
+
+
+
+    </Fragment>
   )
 }
